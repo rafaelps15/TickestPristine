0e804e9fdf8e15d8db3d246ca4e0af9841de9a0a
Rafael Santos <rafaelps15@hotmail.com>
2025-08-11 22:49:03 -0300

    refactor(swagger): mover configuração para extension method e limitar a development

---
+++ WebApi/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace WebApi.Extensions;
+
+public static class SwaggerExtensions
+{
+    public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Tickest",
+                    Version = "v1"
+                });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header usando o esquema Bearer.
+                      Digite 'Bearer [espaço] e então seu token no campo abaixo.
+                      Exemplo: 'Bearer abcdef12345'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+                    },
+                    new List<String>()
+                    }
+                });
+            });
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "TickestPristine");
+            c.RoutePrefix = "swagger";
+        });
+
+        return app;
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,12 +1,11 @@
 using Application;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
 using Serilog;
 using Tickest.Infrastructure;
 using Tickest.Infrastructure.Authentication;
 using Tickest.Infrastructure.Mvc.Middlewares;
 using Tickest.Persistence;
 using Tickest.Persistence.Seeders;
+using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,38 +14,8 @@ builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSet
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Tickest", Version = "v1" });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = @"JWT Authorization header usando o esquema Bearer.
-                      Digite 'Bearer [espaço] e então seu token no campo abaixo.
-                      Exemplo: 'Bearer abcdef12345'",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
 
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-    {
-        {
-        new OpenApiSecurityScheme
-        {
-            Reference = new OpenApiReference
-            {
-                Type = ReferenceType.SecurityScheme,
-                Id = "Bearer"
-            },
-            Scheme = "oauth2",
-            Name = "Bearer",
-            In = ParameterLocation.Header,
-        },
-        new List<String>()
-        }
-    });
-});
+builder.Services.AddSwaggerDocumentation();
 
 
 // Registrar serviços customizados via extensões (Application, Infrastructure, Persistence)
@@ -74,14 +43,12 @@ await app.Services.RunDatabaseSeedingAsync();
 // Middleware
 app.UseCors("DefaultPolicy");
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Usa sua extensão para habilitar Swagger (independente do ambiente)
+app.UseSwaggerConfiguration();
 
 app.UseMiddleware<ErrorHandlerMiddleware>();
 app.UseSerilogRequestLogging();
+
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
